08c6c4741d3b51bac28ebd332c65c97e054c4210
keremceylan <keremceylan14@gmail.com>
2025-03-17 15:18:56 +0300

    KC deneme

--- src/test/java/deneme.java
+++ src/test/java/deneme.java
@@ -1,5 +1,10 @@
 public class deneme {
     public static void main(String[] args) {
         System.out.println("merhaba");
+
+        int a = 15;
+        int b = 20;
+
+        System.out.println(a+b);
     }
 }
